7ac852705303c99d01e4d901c7a9d837677b11d8
belousov <belousovsergej56@gmail.com>
2026-05-26 18:40:42 +0300

    tests

--- main.go
+++ main.go
@@ -3,8 +3,25 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "tn/cmd"
+import (
+	// "tn/cmd"
+	"fmt"
+	"path/filepath"
+
+	// "runtime"
+	"tn/internal"
+)
 
 func main() {
-	cmd.Execute()
+	// fmt.Println(runtime.GOOS)
+	file := internal.GetFile()
+	// internal.EditFile(file)
+	valuteName := filepath.Base(internal.Config().MainValut)
+	// fmt.Println(valuteName)
+	err := internal.OpenURI(valuteName, file)
+	if err != nil {
+		fmt.Println("Ошибка при открытии Obsidian:", err)
+	}
+	// cmd.Execute()
+
 }
